9035678680a0c8d517c1363d4dd26a70df6fe9e7
v.lukyanenko <v.lukyanenko@pixelplex.io>
2020-07-02 16:54:25 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import reactDOM from 'react-dom';
+// import './styles/style.css';
+import './styles/style.scss';
+
+
+const App = () => {
+  return (
+    <h1>Hello a</h1>
+  )
+}
+
+reactDOM.render(<App/>, document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    hot: true,
+  },
+};
